dfe7c4601b5a902b05ea6de2c719ade49ff0bfa3
SYED DILAWAR HUSSAIN <syeddilawar4848@gmail.com>
2023-03-26 11:55:37 +0500

    first class Introducation to TypeScript and basic example

--- Class 1/app.ts
+++ Class 1/app.ts
@@ -11,3 +11,19 @@ button.addEventListener("click", function () {
   let secondInput = input2.value;
   console.log(add(+firstInput, +secondInput));
 });
+
+//Basic Types
+// 1 String : string => "dilawar","hussain"
+// 2 number : number => 0,-20,4.5
+// 3 boolean : boolean => true false
+
+// Example;
+//string Example
+// let person: string;
+// person = "type String";
+
+// array example
+// let users: string[];
+// users = ["dilawar", "bilal", "hassnain", "azeem"];
+// users.push("20");
+// users.push();
