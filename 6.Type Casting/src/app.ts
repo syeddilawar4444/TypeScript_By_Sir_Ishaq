97a93328ec02d8a9b53bf745add61d3e3241d9f5
SYED DILAWAR HUSSAIN <syeddilawar4848@gmail.com>
2023-04-04 23:18:09 +0500

    Type Casting

---
+++ 6.Type Casting/src/app.ts
@@ -0,0 +1,6 @@
+// Class 6
+// TYPE CASTING
+// type Casting two method
+const input = document.getElementById("num1")! as HTMLElement
+const input2 = <HTMLInputElement>document.getElementById("num2") //recommended way for typeScript Documentation
+
